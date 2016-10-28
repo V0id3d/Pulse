7575d97998144ce3893613ffcc23f3bec08f838a
Carlos Quinones <CQuinones@Qzion.net>
2016-10-28 15:25:03 -0400

    Created Component qz-alertcontainer as well as added SCSS Link rules.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -52,6 +52,9 @@
             </ul>
         </div>
     </nav>
+    <qz-alertcontainer position="top-right">
+        {{--Insert Error Handling Here--}}
+    </qz-alertcontainer>
     @yield('PageContent')
 </div>
 
